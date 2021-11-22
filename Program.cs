566171eedfcdc32bc0661263eae3292ef1845111
Татьяна Анисимова <kulmina@mail.ru>
2021-11-22 20:34:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string fraze = Console.ReadLine();
+            string[] stringArray = fraze.Split();
+            string rusulString = "";
+            foreach (string s in stringArray)
+            {
+                rusulString += s.Substring(0, 1).ToLower() + s.Substring(1) + ' ';
+                
+            }
+            
+            rusulString = rusulString.Replace(" ","");
+            Console.WriteLine(rusulString);
+            char[] letters = rusulString.ToArray();
+            int len = letters.Length;
+            for (int i = 0; i < len/2; ++i)
+            {
+                if (letters[i] == letters[len - i - 1])
+                    Console.WriteLine("Фраза является палиндромом");
+                else
+                    Console.WriteLine("Фраза не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+//А роза упала на лапу Азора
